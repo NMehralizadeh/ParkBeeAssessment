c6fdf0730bfd7b2d5e58781397a03f41ab5d77e3
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-06-05 15:03:37 +0430

    Add name to Door entity

--- ParkBee.Assessment.Domain/Entities/Door.cs
+++ ParkBee.Assessment.Domain/Entities/Door.cs
@@ -5,6 +5,7 @@ namespace ParkBee.Assessment.Domain.Entities
     public class Door
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public int GarageId { get; set; }
         public string IP { get; set; }      
         public Garage Garage { get; set; }

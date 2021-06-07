33ae0012fa06c9e2ccbfad38893dc77d49a99559
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-06-07 10:10:39 +0430

    Remove Unused Using

--- ParkBee.Assessment.Application/Services/CheckDoorsStatusesJob.cs
+++ ParkBee.Assessment.Application/Services/CheckDoorsStatusesJob.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ParkBee.Assessment.Application.Interfaces;
-using ParkBee.Assessment.Domain.Entities;
 
 namespace ParkBee.Assessment.Application.Services
 {

dc9ff949afad6a3b4fd40bef4360e8e2f446f343
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-06-07 23:56:06 +0430

    Add Test for Garage Command & Query

---
+++ ParkBee.Assessment.Application.UnitTests/Garages/GetGarageDetailsQueryTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ParkBee.Assessment.Application.Garages.Queries.GetGarageDetails;
+using ParkBee.Assessment.Application.Interfaces;
+using ParkBee.Assessment.Application.UnitTests.Common;
+using ParkBee.Assessment.Persistence;
+using Xunit;
+using Moq;
+
+namespace ParkBee.Assessment.Application.UnitTests.Garages
+{
+    public class GetGarageDetailsQueryTests : IDisposable
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly Mock<ILoggedInUserContext> _loggedInUserContextMock;
+
+        public GetGarageDetailsQueryTests()
+        {
+            _loggedInUserContextMock = new Mock<ILoggedInUserContext>();
+            _loggedInUserContextMock.Setup(m => m.GarageId).Returns(2);
+            _dbContext = ApplicationDbContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            ApplicationDbContextFactory.Destroy(_dbContext);
+        }
+
+        [Fact]
+        public async Task ShouldReturnGarageDetails()
+        {
+            var sut = new GetGarageDetailsQueryHandler(_dbContext, _loggedInUserContextMock.Object);
+
+            var result = await sut.Handle(new GetGarageDetailsQuery(), CancellationToken.None);
+            Assert.Equal(2, result.GarageId);
+            Assert.Equal(2, result.Doors.Count());
+            Assert.IsType<GarageDto>(result);
+
+        }
+    }
+}
---
+++ ParkBee.Assessment.Application.UnitTests/Garages/RefreshDoorStatusCommandTests.cs
@@ -0,0 +1,55 @@
+using Moq;
+using Xunit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ParkBee.Assessment.Persistence;
+using ParkBee.Assessment.Domain.Entities;
+using ParkBee.Assessment.Application.Interfaces;
+using ParkBee.Assessment.Application.Garages.Commands;
+using ParkBee.Assessment.Application.UnitTests.Common;
+
+namespace ParkBee.Assessment.Application.UnitTests.Garages
+{
+    public class RefreshDoorStatusCommandTests : IDisposable
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+        private readonly Mock<IDoorStatusService> _doorStatusServiceMock;
+        private readonly Mock<ILoggedInUserContext> _loggedInUserContextMock;
+
+        public RefreshDoorStatusCommandTests()
+        {
+            _loggedInUserContextMock = new Mock<ILoggedInUserContext>();
+            _loggedInUserContextMock.Setup(m => m.GarageId).Returns(2);
+            _dbContext = ApplicationDbContextFactory.Create();
+
+            _doorStatusServiceMock = new Mock<IDoorStatusService>();
+        }
+
+        public void Dispose()
+        {
+            ApplicationDbContextFactory.Destroy(_dbContext);
+        }
+         
+        [Fact]
+        public void ShouldCheckRequiredInjects()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RefreshDoorStatusCommandHandler(null, _doorStatusServiceMock.Object, _loggedInUserContextMock.Object));
+            Assert.Throws<ArgumentNullException>(() => new RefreshDoorStatusCommandHandler(_dbContext, null, _loggedInUserContextMock.Object));
+            Assert.Throws<ArgumentNullException>(() => new RefreshDoorStatusCommandHandler(_dbContext, _doorStatusServiceMock.Object, null));
+        }
+
+        [Fact]
+        public async Task ShouldReturnDoorStatus()
+        {
+            _doorStatusServiceMock.Setup(cs => cs.CheckDoorStatus(It.Is<Door>(d => d.Id == 2))).ReturnsAsync(false);
+            _doorStatusServiceMock.Setup(cs => cs.CheckDoorStatus(It.Is<Door>(d => d.Id == 3))).ReturnsAsync(true);
+            var sut = new RefreshDoorStatusCommandHandler(_dbContext, _doorStatusServiceMock.Object, _loggedInUserContextMock.Object);
+
+
+            Assert.False(await sut.Handle(new RefreshDoorStatusCommand { DoorId = 2 }, CancellationToken.None));
+            Assert.True(await sut.Handle(new RefreshDoorStatusCommand { DoorId = 3 }, CancellationToken.None));
+        }
+    }
+}

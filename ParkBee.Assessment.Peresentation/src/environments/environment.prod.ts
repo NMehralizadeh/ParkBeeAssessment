8d92960a482b3338286a06761b7869ff5b578a0a
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-06-09 21:13:25 +0430

    Add Token to Environment file

--- ParkBee.Assessment.Peresentation/src/environments/environment.prod.ts
+++ ParkBee.Assessment.Peresentation/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:'http://localhost:49835/'
+  baseUrl: 'http://localhost:49835/',
+  token: '',
 };
--- ParkBee.Assessment.Peresentation/src/environments/environment.ts
+++ ParkBee.Assessment.Peresentation/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl:'http://localhost:49835/',
-  token : ''
+  baseUrl: 'http://localhost:49835/',
+  token: '',
 };
